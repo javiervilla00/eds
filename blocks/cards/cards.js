ccfabcc1b91b1172493a0cc2c0335524680d38c2
javiervilla00 <javiervilla2000.jv@gmail.com>
2026-05-29 12:59:41 +0200

    add cards blocks

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,17 +1,44 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
-  /* change to ul, li */
   const ul = document.createElement('ul');
+  ul.classList.add('cards-list');
+
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
-    while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
-    });
+    li.classList.add('cards-card');
+
+    const [imageCell, titleCell, authorCell] = row.children;
+
+    const picture = imageCell?.querySelector('picture, img');
+    if (picture) {
+      const imageWrapper = document.createElement('div');
+      imageWrapper.classList.add('cards-card-image');
+      imageWrapper.append(picture);
+      li.append(imageWrapper);
+    }
+
+    const content = document.createElement('div');
+    content.classList.add('cards-card-content');
+
+    if (titleCell) {
+      const h2 = document.createElement('h2');
+      h2.classList.add('cards-card-title');
+      h2.textContent = titleCell.textContent.trim();
+      content.append(h2);
+    }
+
+    if (authorCell) {
+      const author = document.createElement('span');
+      author.classList.add('cards-card-author');
+      author.textContent = `By ${authorCell.textContent.trim()}`;
+      content.append(author);
+    }
+
+    li.append(content);
     ul.append(li);
   });
-  ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
-  block.replaceChildren(ul);
+
+  block.textContent = '';
+  block.append(ul);
 }
