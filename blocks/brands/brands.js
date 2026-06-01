386c13012c043e809fa9ca64ff571a5f4b710a07
javiervilla00 <javiervilla2000.jv@gmail.com>
2026-06-01 08:26:41 +0200

    add brands block

---
+++ blocks/brands/brands.js
@@ -0,0 +1,20 @@
+import { createOptimizedPicture } from '../../scripts/aem.js';
+
+export default function decorate(block) {
+  const row = block.firstElementChild;
+  const wrapper = document.createElement('div');
+  wrapper.classList.add('brands-list');
+
+  [...row.children].forEach((cell) => {
+    const img = cell.querySelector('picture, img');
+    if (img) {
+      const item = document.createElement('div');
+      item.classList.add('brands-item');
+      item.append(img.closest('picture') ?? img);
+      wrapper.append(item);
+    }
+  });
+
+  block.textContent = '';
+  block.append(wrapper);
+}
